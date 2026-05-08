95743b8983dbba96cbd4c94c304242000b21226f
initchu <livecityccz@gmail.com>
2026-05-08 04:17:04 +0000

    chore: upgrade PyYAML to 6.0.1

---
+++ src/src_k2ks.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+from typing import List
+
+
+@dataclass
+class Config:
+    host: str = "localhost"
+    port: int = 8080
+    tags: List[str] = field(default_factory=list)
+    debug: bool = False
+
+    def endpoint(self) -> str:
+        return f"http://{self.host}:{self.port}"
+
+# 2026-05-08 04:17:04
