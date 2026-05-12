aff7a3547119ddc5b9d92668e2b448a1aafa6a97
initchu <livecityccz@gmail.com>
2026-05-12 08:44:12 +0000

    test: add unit tests for HMAC signing helper

--- examples/examples_exxq.py
+++ examples/examples_exxq.py
@@ -11,4 +11,4 @@ def save_json(data: dict, path, indent: int = 2) -> None:
     with open(path, "w", encoding="utf-8") as fh:
         json.dump(data, fh, ensure_ascii=False, indent=indent)
 
-# 2026-04-28 06:16:56
+# 2026-05-12 08:44:12
