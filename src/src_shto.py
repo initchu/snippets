22d54e7055f1f3e154dc6420495eb3fa04f16d0b
initchu <livecityccz@gmail.com>
2026-07-10 04:53:16 +0000

    feat: support JSON load/save with UTF-8 encoding

--- src/src_shto.py
+++ src/src_shto.py
@@ -11,4 +11,4 @@ def save_json(data: dict, path, indent: int = 2) -> None:
     with open(path, "w", encoding="utf-8") as fh:
         json.dump(data, fh, ensure_ascii=False, indent=indent)
 
-# 2026-05-09 06:10:52
+# 2026-07-10 04:53:16
